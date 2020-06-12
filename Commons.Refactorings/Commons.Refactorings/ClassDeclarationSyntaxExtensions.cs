8b07315d2c9f983eeb9699a695bdd16534507b06
Rafael Teixeira <monoman@gmail.com>
2020-06-12 11:31:32 -0300

    Push local work, but need to rethink the way ahead

--- Commons.Refactorings/Commons.Refactorings/ClassDeclarationSyntaxExtensions.cs
+++ Commons.Refactorings/Commons.Refactorings/ClassDeclarationSyntaxExtensions.cs
@@ -32,136 +32,141 @@ using Microsoft.CodeAnalysis.Formatting;
 
 namespace Commons.Refactorings
 {
-    public static class ClassDeclarationSyntaxExtensions
-    {
-        public const int MaximumNumberOfMembers = 12;
+	public static class ClassDeclarationSyntaxExtensions
+	{
+		public const int MaximumNumberOfMembers = 12;
 
-        public static ClassDeclarationSyntax AddPartialModifier(this ClassDeclarationSyntax classDecl)
-            => classDecl.IsPartial() ? classDecl : classDecl.InsertPartialFragment();
+		public static ClassDeclarationSyntax AddPartialModifier(this ClassDeclarationSyntax classDecl)
+			=> classDecl.IsPartial() ? classDecl : classDecl.InsertPartialFragment();
 
-        public static async Task<SyntaxNode> AsNewCompilationUnitAsync(this ClassDeclarationSyntax classDecl, Document document, CancellationToken cancellationToken)
-        {
-            var newRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
-            var nodesToRemove = classDecl.FilterSiblingsInNamespace(n => !n.Equals(classDecl));
-            var cleanedRoot = newRoot.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.KeepNoTrivia);
-            return Formatter.Format(cleanedRoot, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
-        }
+		public static async Task<SyntaxNode> AsNewCompilationUnitAsync(this ClassDeclarationSyntax classDecl, Document document, CancellationToken cancellationToken)
+		{
+			var newRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+			var nodesToRemove = classDecl.FilterSiblingsInNamespace(n => !n.Equals(classDecl));
+			var cleanedRoot = newRoot.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.KeepNoTrivia);
+			return Formatter.Format(cleanedRoot, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
+		}
 
-        public static List<ClassDeclarationSyntax> Break(this ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
-        {
-            var partials = new List<ClassDeclarationSyntax>();
-            classDecl = classDecl.RemoveRegions();
-            var allMembers = classDecl.Members.ToList();
-            var allMembersCount = allMembers.Count;
-            if (allMembersCount <= MaximumNumberOfMembers)
-            {
-                partials.Add(classDecl);
-                return partials;
-            }
-            classDecl = classDecl.AddPartialModifier();
-            allMembers = classDecl.Members.ToList();
-            allMembersCount = allMembers.Count;
-            var first = classDecl.RemoveMembers(allMembers.Skip(MaximumNumberOfMembers));
-            partials.Add(first.WithoutTrailingTrivia());
-            var membersMoved = MaximumNumberOfMembers;
-            while (!(cancellationToken.IsCancellationRequested || membersMoved >= allMembersCount))
-            {
-                var membersToInsert = allMembers.Skip(membersMoved).Take(MaximumNumberOfMembers).ToArray();
-                var tree = SyntaxFactory.ParseSyntaxTree($"{classDecl.Modifiers} class {classDecl.Identifier} {CLASS_BODY_TEMPLATE}");
-                var nextPartial = tree.GetRoot(cancellationToken).DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
-                nextPartial = nextPartial.InsertNodesAfter(nextPartial.DescendantNodes().First(), membersToInsert);
-                nextPartial = nextPartial.RemoveNode(nextPartial.DescendantNodes().First(), SyntaxRemoveOptions.KeepNoTrivia);
-                partials.Add(nextPartial.WithLeadingTrivia(SyntaxFactory.LineFeed, SyntaxFactory.LineFeed));
-                membersMoved += MaximumNumberOfMembers;
-            }
-            return partials;
-        }
+		public static List<ClassDeclarationSyntax> Break(this ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
+		{
+			var partials = new List<ClassDeclarationSyntax>();
+			classDecl = classDecl.RemoveRegions();
+			var allMembers = classDecl.Members.ToList();
+			var allMembersCount = allMembers.Count;
+			if (allMembersCount <= MaximumNumberOfMembers)
+			{
+				partials.Add(classDecl);
+				return partials;
+			}
+			classDecl = classDecl.AddPartialModifier();
+			allMembers = classDecl.Members.ToList();
+			allMembersCount = allMembers.Count;
+			var first = classDecl.RemoveMembers(allMembers.Skip(MaximumNumberOfMembers));
+			partials.Add(first.WithoutTrailingTrivia());
+			var membersMoved = MaximumNumberOfMembers;
+			while (!(cancellationToken.IsCancellationRequested || membersMoved >= allMembersCount))
+			{
+				var membersToInsert = allMembers.Skip(membersMoved).Take(MaximumNumberOfMembers).ToArray();
+				var tree = SyntaxFactory.ParseSyntaxTree($"{classDecl.Modifiers} class {classDecl.Identifier} {CLASS_BODY_TEMPLATE}");
+				var nextPartial = tree.GetRoot(cancellationToken).DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+				nextPartial = nextPartial.InsertNodesAfter(nextPartial.DescendantNodes().First(), membersToInsert);
+				nextPartial = nextPartial.RemoveNode(nextPartial.DescendantNodes().First(), SyntaxRemoveOptions.KeepNoTrivia);
+				partials.Add(nextPartial.WithLeadingTrivia(SyntaxFactory.LineFeed, SyntaxFactory.LineFeed));
+				membersMoved += MaximumNumberOfMembers;
+			}
+			return partials;
+		}
 
-        public static List<ClassDeclarationSyntax> Break(this ClassDeclarationSyntax classDecl, MemberDeclarationSyntax splitMethod, CancellationToken cancellationToken)
-        {
-            try
-            {
-                var partials = new List<ClassDeclarationSyntax>();
-                classDecl = classDecl.RemoveRegions();
-                var allMembers = classDecl.Members.ToList();
-                var allMembersCount = allMembers.Count;
-                if (allMembersCount <= 1)
-                {
-                    partials.Add(classDecl);
-                    return partials;
-                }
-                classDecl = classDecl.AddPartialModifier();
-                allMembers = classDecl.Members.ToList();
-                if (!(cancellationToken.IsCancellationRequested))
-                {
-                    var tail = allMembers.SkipWhile(m => !m.IsEquivalentTo(splitMethod)).ToArray();
-                    var first = classDecl.RemoveMembers(tail);
-                    partials.Add(first.WithoutTrailingTrivia());
-                    var tree = SyntaxFactory.ParseSyntaxTree($"{classDecl.Modifiers} class {classDecl.Identifier} {CLASS_BODY_TEMPLATE}");
-                    var nextPartial = tree.GetRoot(cancellationToken).DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
-                    nextPartial = nextPartial.InsertNodesAfter(nextPartial.DescendantNodes().First(), tail);
-                    nextPartial = nextPartial.RemoveNode(nextPartial.DescendantNodes().First(), SyntaxRemoveOptions.KeepNoTrivia);
-                    partials.Add(nextPartial.WithLeadingTrivia(SyntaxFactory.LineFeed, SyntaxFactory.LineFeed));
-                }
-                return partials;
-            } catch (Exception)
-            {
-                var partials = new List<ClassDeclarationSyntax>();
-                partials.Add(classDecl);
-                return partials;
-            }
-        }
+		public static List<ClassDeclarationSyntax> Break(this ClassDeclarationSyntax classDecl, MemberDeclarationSyntax splitMethod, CancellationToken cancellationToken)
+		{
+			try
+			{
+				var partials = new List<ClassDeclarationSyntax>();
+				classDecl = classDecl.RemoveRegions();
+				var allMembers = classDecl.Members.ToList();
+				var allMembersCount = allMembers.Count;
+				if (allMembersCount <= 1)
+				{
+					partials.Add(classDecl);
+					return partials;
+				}
+				classDecl = classDecl.AddPartialModifier();
+				allMembers = classDecl.Members.ToList();
+				if (!(cancellationToken.IsCancellationRequested))
+				{
+					var tail = allMembers.SkipWhile(m => !m.IsEquivalentTo(splitMethod)).ToArray();
+					var first = classDecl.RemoveMembers(tail);
+					partials.Add(first.WithoutTrailingTrivia());
+					var tree = SyntaxFactory.ParseSyntaxTree($"{classDecl.Modifiers} class {classDecl.Identifier} {CLASS_BODY_TEMPLATE}");
+					var nextPartial = tree.GetRoot(cancellationToken).DescendantNodes().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+					nextPartial = nextPartial.InsertNodesAfter(nextPartial.DescendantNodes().First(), tail);
+					nextPartial = nextPartial.RemoveNode(nextPartial.DescendantNodes().First(), SyntaxRemoveOptions.KeepNoTrivia);
+					partials.Add(nextPartial.WithLeadingTrivia(SyntaxFactory.LineFeed, SyntaxFactory.LineFeed));
+				}
+				return partials;
+			}
+			catch (Exception)
+			{
+				var partials = new List<ClassDeclarationSyntax>();
+				partials.Add(classDecl);
+				return partials;
+			}
+		}
 
-        public static string BuildNewPartialSourceName(this ClassDeclarationSyntax classDecl)
-        {
-            var methodNode = (MethodDeclarationSyntax)classDecl.Members.FirstOrDefault(m => m is MethodDeclarationSyntax);
-            var suffix = methodNode?.Identifier.Text ?? "Partial";
-            return classDecl.Identifier.Text + '-' + suffix;
-        }
+		public static string BuildNewPartialSourceName(this ClassDeclarationSyntax classDecl)
+		{
+			var methodNode = (MethodDeclarationSyntax)classDecl.Members.FirstOrDefault(m => m is MethodDeclarationSyntax);
+			var suffix = methodNode?.Identifier.Text ?? "Partial";
+			return classDecl.Identifier.Text + '-' + suffix;
+		}
 
-        public static IEnumerable<SyntaxNode> FilterSiblingsInNamespace(this ClassDeclarationSyntax classDecl, Func<SyntaxNode, bool> filter) => (classDecl.Parent as NamespaceDeclarationSyntax)?.Members.Where(filter) ?? EmptyList;
+		public static IEnumerable<SyntaxNode> FilterSiblingsInNamespace(this ClassDeclarationSyntax classDecl, Func<SyntaxNode, bool> filter) => (classDecl.Parent as NamespaceDeclarationSyntax)?.Members.Where(filter) ?? EmptyList;
 
-        public static bool HasManyPartialsInSameSource(this ClassDeclarationSyntax classDecl) => classDecl.IsPartial() && CountOfPartialSiblings(classDecl) > 1;
+		public static bool HasManyInSameSource(this ClassDeclarationSyntax classDecl) => CountOfSiblings(classDecl) > 1;
 
-        public static bool HasTooManyMembers(this ClassDeclarationSyntax classDecl) => classDecl.Members.Count > MaximumNumberOfMembers;
+		public static bool HasManyPartialsInSameSource(this ClassDeclarationSyntax classDecl) => classDecl.IsPartial() && CountOfPartialSiblings(classDecl) > 1;
 
-        private static readonly string CLASS_BODY_TEMPLATE = @"
+		public static bool HasTooManyMembers(this ClassDeclarationSyntax classDecl) => classDecl.Members.Count > MaximumNumberOfMembers;
+
+		private static readonly string CLASS_BODY_TEMPLATE = @"
 {private int dummy;}
 
 ";
 
-        private static readonly List<SyntaxNode> EmptyList = new List<SyntaxNode>();
+		private static readonly List<SyntaxNode> EmptyList = new List<SyntaxNode>();
 
-        private static int CountOfPartialSiblings(ClassDeclarationSyntax classDecl) => classDecl.FilterSiblingsInNamespace(n => HasSameName(n, classDecl)).Count();
+		private static int CountOfPartialSiblings(ClassDeclarationSyntax classDecl) => classDecl.FilterSiblingsInNamespace(n => HasSameName(n, classDecl)).Count();
 
-        private static bool HasSameName(SyntaxNode node, ClassDeclarationSyntax classDecl)
-        {
-            var typeDeclarationSyntax = node as ClassDeclarationSyntax;
-            return typeDeclarationSyntax.IsPartial() && typeDeclarationSyntax?.Identifier.Text == classDecl.Identifier.Text;
-        }
+		private static int CountOfSiblings(ClassDeclarationSyntax classDecl) => classDecl.FilterSiblingsInNamespace(n => true).Count();
 
-        private static ClassDeclarationSyntax InsertPartialFragment(this ClassDeclarationSyntax classDecl)
-            => classDecl.AddModifiers(SyntaxFactory.ParseTokens("partial").First());
+		private static bool HasSameName(SyntaxNode node, ClassDeclarationSyntax classDecl)
+		{
+			var typeDeclarationSyntax = node as ClassDeclarationSyntax;
+			return typeDeclarationSyntax.IsPartial() && typeDeclarationSyntax?.Identifier.Text == classDecl.Identifier.Text;
+		}
 
-        private static bool IsPartial(this ClassDeclarationSyntax classDecl)
-        {
-            if (classDecl == null)
-                return false;
-            return classDecl.Modifiers.Any(SyntaxKind.PartialKeyword);
-        }
+		private static ClassDeclarationSyntax InsertPartialFragment(this ClassDeclarationSyntax classDecl)
+			=> classDecl.AddModifiers(SyntaxFactory.ParseTokens("partial").First());
 
-        private static ClassDeclarationSyntax RemoveDescendantsOfType<T>(this ClassDeclarationSyntax classDecl) where T : SyntaxNode
-        {
-            if (classDecl == null)
-                return classDecl;
-            var nodesToRemove = classDecl.DescendantNodes(descendIntoTrivia: true).OfType<T>();
-            return nodesToRemove.Any() ? classDecl.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.KeepNoTrivia) : classDecl;
-        }
+		private static bool IsPartial(this ClassDeclarationSyntax classDecl)
+		{
+			if (classDecl == null)
+				return false;
+			return classDecl.Modifiers.Any(SyntaxKind.PartialKeyword);
+		}
 
-        private static ClassDeclarationSyntax RemoveMembers(this ClassDeclarationSyntax classDecl, IEnumerable<MemberDeclarationSyntax> methods)
-            => classDecl.RemoveNodes(methods, SyntaxRemoveOptions.KeepNoTrivia);
+		private static ClassDeclarationSyntax RemoveDescendantsOfType<T>(this ClassDeclarationSyntax classDecl) where T : SyntaxNode
+		{
+			if (classDecl == null)
+				return classDecl;
+			var nodesToRemove = classDecl.DescendantNodes(descendIntoTrivia: true).OfType<T>();
+			return nodesToRemove.Any() ? classDecl.RemoveNodes(nodesToRemove, SyntaxRemoveOptions.KeepNoTrivia) : classDecl;
+		}
 
-        private static ClassDeclarationSyntax RemoveRegions(this ClassDeclarationSyntax classDecl)
-            => classDecl.RemoveDescendantsOfType<RegionDirectiveTriviaSyntax>().RemoveDescendantsOfType<EndRegionDirectiveTriviaSyntax>();
-    }
+		private static ClassDeclarationSyntax RemoveMembers(this ClassDeclarationSyntax classDecl, IEnumerable<MemberDeclarationSyntax> methods)
+			=> classDecl.RemoveNodes(methods, SyntaxRemoveOptions.KeepNoTrivia);
+
+		private static ClassDeclarationSyntax RemoveRegions(this ClassDeclarationSyntax classDecl)
+			=> classDecl.RemoveDescendantsOfType<RegionDirectiveTriviaSyntax>().RemoveDescendantsOfType<EndRegionDirectiveTriviaSyntax>();
+	}
 }
--- Commons.Refactorings/Commons.Refactorings/CodeRefactoringProvider.cs
+++ Commons.Refactorings/Commons.Refactorings/CodeRefactoringProvider.cs
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using System;
 using System.Composition;
 using System.Linq;
 using System.Threading;
@@ -32,98 +33,112 @@ using Microsoft.CodeAnalysis.Formatting;
 
 namespace Commons.Refactorings
 {
-    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(CodeRefactoringProvider)), Shared]
-    public class CodeRefactoringProvider : Microsoft.CodeAnalysis.CodeRefactorings.CodeRefactoringProvider
-    {
-        public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
-        {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+	[ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(CodeRefactoringProvider)), Shared]
+	public class CodeRefactoringProvider : Microsoft.CodeAnalysis.CodeRefactorings.CodeRefactoringProvider
+	{
+		public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // Find the node at the selection.
-            var node = root.FindNode(context.Span);
+			// Find the node at the selection.
+			var node = root.FindNode(context.Span);
 
-            var classDecl = node as ClassDeclarationSyntax;
-            if (classDecl == null || IsMemberOfSomeClass(classDecl))
-            {
-                var memberDecl = node as MemberDeclarationSyntax;
-                if (memberDecl != null && IsMemberOfSomeClass(memberDecl))
-                {
-                    var action = CodeAction.Create("Split here to a partial", c => Action_BreakIntoPartialFromMemberAsync(context.Document, memberDecl, c));
-                    context.RegisterRefactoring(action);
-                }
-                return;
-            }
-            if (classDecl.HasTooManyMembers())
-            {
-                var action = CodeAction.Create("Break into partials", c => Action_BreakIntoPartialsAsync(context.Document, classDecl, c));
-                context.RegisterRefactoring(action);
-            }
-            if (classDecl.HasManyPartialsInSameSource())
-            {
-                var action = CodeAction.Create("Move partial to new source file", c => Action_SeparatePartialAsync(context.Document, classDecl, c));
-                context.RegisterRefactoring(action);
-            }
-        }
+			var classDecl = node as ClassDeclarationSyntax;
+			if (classDecl == null || IsMemberOfSomeClass(classDecl))
+			{
+				var memberDecl = node as MemberDeclarationSyntax;
+				if (memberDecl != null && IsMemberOfSomeClass(memberDecl))
+				{
+					var action = CodeAction.Create("Split here to a partial", c => Action_BreakIntoPartialFromMemberAsync(context.Document, memberDecl, c));
+					context.RegisterRefactoring(action);
+				}
+				return;
+			}
+			if (classDecl.HasTooManyMembers())
+			{
+				var action = CodeAction.Create("Break into partials", c => Action_BreakIntoPartialsAsync(context.Document, classDecl, c));
+				context.RegisterRefactoring(action);
+			}
+			if (classDecl.HasManyPartialsInSameSource())
+			{
+				var action = CodeAction.Create("Move partial to new source file", c => Action_SeparatePartialAsync(context.Document, classDecl, c));
+				context.RegisterRefactoring(action);
+			}
+			if (classDecl.HasManyInSameSource())
+			{
+				var action = CodeAction.Create("Move class to new source file", c => Action_SeparateClassAsync(context.Document, classDecl, c));
+				context.RegisterRefactoring(action);
+			}
+		}
 
-        private async static Task<Solution> Action_BreakIntoPartialFromMemberAsync(Document document, MemberDeclarationSyntax memberDecl, CancellationToken cancellationToken)
-        {
-            var classDecl = FindParentClassDeclaration(memberDecl);
-            var partials = classDecl.Break(memberDecl, cancellationToken);
-            return await document.Update(cancellationToken,
-                root =>
-                {
-                    var newRoot = root.ReplaceNode(classDecl, partials.First());
-                    var classDeclarations = newRoot.DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>().Where(n => n.Identifier.Text == classDecl.Identifier.Text);
-                    var newTypeDecl = classDeclarations.FirstOrDefault(n => n.SpanStart >= classDecl.SpanStart) ?? classDeclarations.FirstOrDefault();
-                    var rootNode = newTypeDecl == null ? newRoot : newRoot.InsertNodesAfter(newTypeDecl, partials.Skip(1));
-                    return Formatter.Format(rootNode, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
-                });
-        }
+		private async static Task<Solution> Action_BreakIntoPartialFromMemberAsync(Document document, MemberDeclarationSyntax memberDecl, CancellationToken cancellationToken)
+		{
+			var classDecl = FindParentClassDeclaration(memberDecl);
+			var partials = classDecl.Break(memberDecl, cancellationToken);
+			return await document.Update(cancellationToken,
+				root =>
+				{
+					var newRoot = root.ReplaceNode(classDecl, partials.First());
+					var classDeclarations = newRoot.DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>().Where(n => n.Identifier.Text == classDecl.Identifier.Text);
+					var newTypeDecl = classDeclarations.FirstOrDefault(n => n.SpanStart >= classDecl.SpanStart) ?? classDeclarations.FirstOrDefault();
+					var rootNode = newTypeDecl == null ? newRoot : newRoot.InsertNodesAfter(newTypeDecl, partials.Skip(1));
+					return Formatter.Format(rootNode, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
+				});
+		}
 
-        private async static Task<Solution> Action_BreakIntoPartialsAsync(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
-        {
-            var partials = classDecl.Break(cancellationToken);
-            return await document.Update(cancellationToken,
-                root =>
-                {
-                    var newRoot = root.ReplaceNode(classDecl, partials.First());
-                    var classDeclarations = newRoot.DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
-                    var newTypeDecl = classDeclarations.FirstOrDefault(n => n.Identifier.Text == classDecl.Identifier.Text);
-                    var rootNode = newTypeDecl == null ? newRoot : newRoot.InsertNodesAfter(newTypeDecl, partials.Skip(1));
-                    return Formatter.Format(rootNode, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
-                });
-        }
+		private async static Task<Solution> Action_BreakIntoPartialsAsync(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
+		{
+			var partials = classDecl.Break(cancellationToken);
+			return await document.Update(cancellationToken,
+				root =>
+				{
+					var newRoot = root.ReplaceNode(classDecl, partials.First());
+					var classDeclarations = newRoot.DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
+					var newTypeDecl = classDeclarations.FirstOrDefault(n => n.Identifier.Text == classDecl.Identifier.Text);
+					var rootNode = newTypeDecl == null ? newRoot : newRoot.InsertNodesAfter(newTypeDecl, partials.Skip(1));
+					return Formatter.Format(rootNode, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
+				});
+		}
 
-        private async static Task<Solution> Action_SeparatePartialAsync(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
-        {
-            var newCompilationUnit = await classDecl.AsNewCompilationUnitAsync(document, cancellationToken);
-            if (newCompilationUnit == null || cancellationToken.IsCancellationRequested)
-                return document.Project.Solution;
-            var newSourceName = classDecl.BuildNewPartialSourceName();
-            var newSolution = await document.Update(cancellationToken,
-                root =>
-                {
-                    var newRoot = root.RemoveNode(classDecl, SyntaxRemoveOptions.KeepNoTrivia);
-                    return Formatter.Format(newRoot, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
-                });
-            if (cancellationToken.IsCancellationRequested)
-                return document.Project.Solution;
-            return document.AddDerivedDocument(newSolution, newSourceName, newCompilationUnit);
-        }
+		private async static Task<Solution> Action_SeparateClassAsync(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
+		{
+			return await ActionInternal_SeparateClassAsync(document, classDecl, cancellationToken, classDecl.Identifier.Text);
+		}
 
-        private static T FindParent<T>(SyntaxNode node) where T : SyntaxNode
-        {
-            if (node == null)
-                return null;
-            if (node is T)
-                return (T)node;
-            return FindParent<T>(node.Parent);
-        }
+		private async static Task<Solution> Action_SeparatePartialAsync(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken)
+		{
+			return await ActionInternal_SeparateClassAsync(document, classDecl, cancellationToken, classDecl.BuildNewPartialSourceName());
+		}
 
-        private static ClassDeclarationSyntax FindParentClassDeclaration(MemberDeclarationSyntax memberDecl)
-            => FindParent<ClassDeclarationSyntax>(memberDecl.Parent);
+		private static async Task<Solution> ActionInternal_SeparateClassAsync(Document document, ClassDeclarationSyntax classDecl, CancellationToken cancellationToken, string newSourceName)
+		{
+			var newCompilationUnit = await classDecl.AsNewCompilationUnitAsync(document, cancellationToken);
+			if (newCompilationUnit == null || cancellationToken.IsCancellationRequested)
+				return document.Project.Solution;
+			var newSolution = await document.Update(cancellationToken,
+				root =>
+				{
+					var newRoot = root.RemoveNode(classDecl, SyntaxRemoveOptions.KeepNoTrivia);
+					return Formatter.Format(newRoot, document.Project.Solution.Workspace, cancellationToken: cancellationToken);
+				});
+			if (cancellationToken.IsCancellationRequested)
+				return document.Project.Solution;
+			return document.AddDerivedDocument(newSolution, newSourceName, newCompilationUnit);
+		}
 
-        private static bool IsMemberOfSomeClass(MemberDeclarationSyntax memberDecl)
-            => FindParentClassDeclaration(memberDecl) != null;
-    }
+		private static T FindParent<T>(SyntaxNode node) where T : SyntaxNode
+		{
+			if (node == null)
+				return null;
+			if (node is T)
+				return (T)node;
+			return FindParent<T>(node.Parent);
+		}
+
+		private static ClassDeclarationSyntax FindParentClassDeclaration(MemberDeclarationSyntax memberDecl)
+			=> FindParent<ClassDeclarationSyntax>(memberDecl.Parent);
+
+		private static bool IsMemberOfSomeClass(MemberDeclarationSyntax memberDecl)
+			=> FindParentClassDeclaration(memberDecl) != null;
+	}
 }
